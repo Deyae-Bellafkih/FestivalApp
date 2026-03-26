cfaa7c8884423e522cb44b21a4fcd1ba598aa87f
bellafkih.22128 <bellafkih.deyae.studente@itispaleocapa.it>
2026-03-26 08:55:52 +0100

    Fase 2

--- FestivalManager.java
+++ FestivalManager.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class FestivalManager {
     public static void main(String[] args) {
         List<String> scaletta = new ArrayList<>();
+        TreeSet<String> ticketCodex = new TreeSet<>();
 
         scaletta.add("Glocky");
         scaletta.add("Lazza");
@@ -18,5 +19,17 @@ public class FestivalManager {
         for (int i = 0; i < scaletta.size(); i++) {
             System.out.println((i+1) + "." + scaletta.get(i));
         }
+
+        ticketCodex.add("TICK-123");
+        ticketCodex.add("TICK-159");
+        ticketCodex.add("TICK-183");
+
+        ticketCodex.add("TICK-123");
+
+        System.out.println("INGRESSI");
+        System.out.println("Tickets:" + ticketCodex);
+        System.out.println("Numero persone entrare" + ticketCodex.size());
+
+
     }
 }
